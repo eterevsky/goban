72b4fc565aed4ce7b05ad10cb41882f89ec236e3
Oleg Eterevsky <oleg@eterevsky.com>
2017-09-16 09:44:25 +0200

    Run a full game.

--- goban/__main__.py
+++ goban/__main__.py
@@ -5,16 +5,51 @@ from . import gtp
 CONFIG_PATH = 'config.json'
 
 
+def run_game(engine1: gtp.GtpEngine, engine2: gtp.GtpEngine):
+    if not engine1.is_running():
+        engine1.run()
+    if not engine2.is_running():
+        engine2.run()
+    engine1.init_game()
+    engine2.init_game()
+
+    player = 0
+    last_move_pass = False
+    while player is not None:
+        move = engine1.genmove('b') if player == 0 else engine2.genmove('w')
+        print('\n{} {}'.format(('b' if player == 0 else 'w'), move))
+
+        if move.lower() == 'resign':
+            score = 'W+Resign' if player == 0 else 'B+Resign'
+            break
+
+        if move.lower() == 'pass':
+            if last_move_pass:
+                score = engine1.final_score()
+                break
+            last_move_pass = True
+        else:
+            last_move_pass = False
+
+        board = engine2.showboard()
+        print(board)
+
+        if player == 0:
+            engine2.play('b', move)
+        else:
+            engine1.play('w', move)
+
+        player = 1 - player
+
+    print('final score: ', score)
+
+
 def main():
     config = json.load(open(CONFIG_PATH))
-    engine = gtp.GtpEngine(args=config['args'])
-    engine.run()
+    engine1 = gtp.GtpEngine(args=config['player1'])
+    engine2 = gtp.GtpEngine(args=config['player2'])
 
-    print(engine.full_name())
-
-    # for i in range(5):
-    #     engine.command('genmove', 'b')
-    #     engine.command('genmove', 'w')
+    run_game(engine1, engine2)
 
 
 main()
--- goban/gtp.py
+++ goban/gtp.py
@@ -1,4 +1,6 @@
+import re
 import subprocess
+from typing import List
 
 
 class GtpError(Exception):
@@ -7,6 +9,17 @@ class GtpError(Exception):
 
 class EngineError(Exception):
     pass
+
+
+RE_WHITESPACE = re.compile(b'\\s')
+
+
+def strip_gtp_line(s: bytes) -> str:
+    """Convert bytes, returned by a GTP engine to a string."""
+    if b'#' in s:
+        s = s[:s.index(b'#')]
+    s = RE_WHITESPACE.sub(b' ', s)
+    return s.rstrip().decode('utf-8')
 
 
 class GtpEngine(object):
@@ -18,25 +31,18 @@ class GtpEngine(object):
         if self._process is not None:
             self.quit()
 
+    def is_running(self):
+        return self._process is not None
+
     def run(self):
         assert self._process is None
         self._process = subprocess.Popen(
             self._args,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
             bufsize=0)
         self.check_protocol_version()
-
-    def full_name(self):
-        return self.name() + ' ' + self.version()
-
-    def init_game(self, size=19, komi=7.5, handicap=0):
-        self.boardsize(size)
-        self.clear_board()
-        self.komi(komi)
-        if handicap:
-            assert 0 < handicap <= 9
-            self.fixed_handicap(handicap)
 
     def command(self, command, *args):
         assert self._process is not None
@@ -47,17 +53,13 @@ class GtpEngine(object):
         command_str = ' '.join(command_parts)
         command_bytes = bytes(command_str + '\n', 'utf-8')
 
-        print('> {}\n'.format(command_str))
         self._process.stdin.write(command_bytes)
         lines = []
         while True:
-            line = self._process.stdout.readline()
-            line = line.strip().decode('utf-8')
+            line = strip_gtp_line(self._process.stdout.readline())
             if not line:
                 break
-            print(line)
             lines.append(line)
-        print()
         assert len(lines) > 0
         status = lines[0][0]
         if status == '=':
@@ -68,6 +70,18 @@ class GtpEngine(object):
             raise EngineError('\n'.join(lines))
         else:
             raise GtpError('Incorrect GTP response:\n', '\n'.join(lines))
+
+    def full_name(self) -> str:
+        return self.name() + ' ' + self.version()
+
+    def init_game(self, size=19, komi=7.5, handicap=0, time=(0, 1, 1)):
+        self.boardsize(size)
+        self.clear_board()
+        self.komi(komi)
+        self.time_settings(*time)
+        if handicap:
+            assert 0 < handicap <= 9
+            self.fixed_handicap(handicap)
 
     def check_protocol_version(self):
         response = self.command('protocol_version')
@@ -83,10 +97,10 @@ class GtpEngine(object):
 
         self._process = None
 
-    def list_commands(self):
+    def list_commands(self) -> List[str]:
         return self.command('list_commands')
 
-    def name(self):
+    def name(self) -> str:
         lines = self.command('name')
         assert len(lines) == 1
         return lines[0]
@@ -108,3 +122,32 @@ class GtpEngine(object):
 
     def fixed_handicap(self, handicap: int):
         self.command('fixed_handicap', str(handicap))
+
+    def play(self, color:str, move: str):
+        self.command('play', color, move)
+
+    def undo(self):
+        self.command('undo')
+
+    def time_settings(self,
+                      main_time: int,
+                      byo_yomi_time: int,
+                      byo_yomi_stones: int):
+        self.command('time_settings',
+                     str(main_time), str(byo_yomi_time), str(byo_yomi_stones))
+
+    # TODO: parse the result
+    def final_score(self) -> str:
+        lines = self.command('final_score')
+        assert len(lines) == 1
+        return lines[0]
+
+    def genmove(self, player) -> str:
+        lines = self.command('genmove', player)
+        assert len(lines) == 1
+        return lines[0]
+
+    def showboard(self) -> str:
+        lines = self.command('showboard')
+        return '\n'.join(lines)
+
